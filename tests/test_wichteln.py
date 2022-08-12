ca0caf31e99decf98f88988720c59aba6433b874
danczw <dev.dc@pm.me>
2022-08-13 00:02:10 +0200

    update patch version

--- tests/test_wichteln.py
+++ tests/test_wichteln.py
@@ -2,7 +2,7 @@ from secret_wichtel import __version__
 from secret_wichtel.secret_wichtel import _calc_secret_pairs
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
 
 def test_shuffle():
     assert _calc_secret_pairs(['Alex', 'Peter', 'Dan']) == 0
